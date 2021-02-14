0f77e546bedb65a49ba7a17cd32776879e67a561
Hristo <karabashevhristo@gmail.com>
2021-02-14 16:01:16 +0200

    clean

--- Lecture_2/while_loop.py
+++
@@ -1,9 +0,0 @@
-# n = input("You're in the Lost Forest. Go left or right? ")
-# while n == "right":
-#     n = input("You're in the Lost Forest. Go left or right? ")
-# print("You got out of the Lost Forest!")
-
-n = 0
-while n < 5:
-   print(n)
-   n = n+1
